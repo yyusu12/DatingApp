44e67e7f8bd4ae5397c040aa22bbd4c73b9b4d11
yyusu12 <yyusuf12@gmial.com>
2020-05-17 16:39:50 +0100

    Initial Commit

---
+++ DatingApp.API/Controllers/valuescontroller.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    //http:localhost:5000/api/values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task <IActionResult> GetValues()
+        {
+            var values = await _context.values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task <IActionResult> GetValue(int id)
+        {
+            var value = await _context.values.FirstOrDefaultAsync(x=>x.id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+      public DataContext(DbContextOptions<DataContext> options) : base(options)
+      {
+
+      }
+
+      public DbSet <Value> values {get; set;}
+
+
+    }
+}
